f6dbed02b6e9f1b1f609352e1cac480c0f04b62e
Ana Espeleta <89929081+anaespeleta115@users.noreply.github.com>
2025-07-07 21:48:05 -0700

    started modularizing e0026 notebooks

---
+++ analysis/scratch/040325-loadData/loadData.R
@@ -0,0 +1,69 @@
+library(tidyverse)
+library(dplyr)
+library(ggplot2)
+library(readr)
+library(ggrepel)
+
+
+
+household_data <- read.table("C:/Users/anaes/OneDrive/UCI_Spring25/rotation/e0026-e0029-e0030.txt", header = TRUE)
+
+
+# Divide dataset into separate tables by experiment. Filter out unnecessary columns
+e0026 <- filter(household_data, str_detect(household_data$sample, "e0026")) %>% 
+  select(sample, biosample1, experiment, passage, OTU, count, replicate, relAbundance, Phylum, Family, Genus)
+
+
+# List the antibiotic-taking subjects.
+# Based on metagenomic and metabolomic data, annotate XHB as antibiotic-taking
+# and XHC as a control subject.
+subjectsAbx <- c("XAA","XBA","XCA","XDA","XEA","XFA","XGA","XHB","XIA","XJA",
+                 "XKA","XLA","XMA","XNA","XOA","XPA","XQA","XRA","XSA","XTA",
+                 "XUA","XVA")
+
+# Extract subject, day, household, and antibiotic information
+e0026 <- e0026 %>%
+  mutate(
+    subject = str_sub(biosample1, 1, -5),
+    day = str_sub(biosample1, -3),
+    household = str_sub(biosample1, 1, -6),
+    antibiotic = if_else(str_sub(biosample1, 1, -5) %in% subjectsAbx, 1, 0)
+  )
+
+
+# Divide up e0026 dataset into separate day datasets
+e0026_day1 <- e0026 %>%  filter(str_detect(biosample1, "001") | str_detect(biosample1, "002") | str_detect(biosample1, "003") | str_detect(biosample1, "022")| str_detect(biosample1, "008")) %>% 
+  mutate(day = "001")
+
+e0026_day29 <- e0026 %>%   filter(str_detect(biosample1, "029") | str_detect(biosample1, "028") | str_detect(biosample1, "027")) %>% 
+  mutate(day = "029")
+
+e0026_day36 <- e0026 %>%  filter(str_detect(biosample1, "036") | str_detect(biosample1, "037")) %>% 
+  mutate(day = "036")
+
+e0026_day64 <- e0026 %>%  filter(str_detect(biosample1, "064")| str_detect(biosample1, "063") | str_detect(biosample1, "072") | str_detect(biosample1, "059")| str_detect(biosample1, "065")) %>% 
+  mutate(day = "064")
+
+# Combine all day datasets
+combined_day_data <- bind_rows(e0026_day1, e0026_day29, e0026_day36, e0026_day64)
+
+# Get all samples that have every passage sequenced
+e0026_all_passages <- e0026 %>%
+  group_by(biosample1)  %>% 
+  mutate(num_passages = n_distinct(passage)) %>% 
+  filter(num_passages == 9) %>% 
+  ungroup() 
+
+# Extract the top 25 families by relative abundance to make plots better
+top_families <- e0026_day29 %>%
+  group_by(Family) %>%
+  summarise(total_abundance = sum(relAbundance, na.rm = TRUE)) %>%
+  arrange(desc(total_abundance)) %>%
+  slice_head(n = 25) %>%
+  pull(Family)
+
+# Define color palette
+my_colors <- readRDS("C:/abx-response-invitro/data/familyColorPalette.rds") 
+
+
+
---
+++ analysis/scratch/040325-plotCompositions/040325-plotCompositions.R
@@ -0,0 +1,23 @@
+### Compositional changes across passages
+
+# Load data
+source("C:/abx-response-invitro/analysis/scratch/040325-loadData/loadData.R")
+
+source("C:/abx-response-invitro/analysis/plotDefaults.R")
+
+# Set output directory
+OUTDIR <- "C:/abx-response-invitro/analysis/scratch/040325-plotCompositions/out/"
+
+# Plot the total relative abundances (which are calculated per community) totaled up for each
+p_compositionsByPassage <- ggplot(e0026_all_passages, aes(x = factor(passage), y = relAbundance, fill = Family)) +
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = my_colors) +
+  labs(title = "Community Composition Across Passages" , x = "Passage Number", y = "Rel. Abundance", fill = "Family") +
+  theme_minimal()+
+  theme(
+    legend.position = "none"
+  ) +
+  facet_wrap(~ biosample1)
+
+
+savePNGPDF(paste0(OUTDIR, "compositionsByPassage"), p_compositionsByPassage, 6, 12)
