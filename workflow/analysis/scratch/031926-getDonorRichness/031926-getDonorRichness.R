bfe9bbd278f93c5fd72550d333fff760911e0d01
Ana Espeleta <anaespeleta115@gmail.com>
2026-03-19 09:10:54 -0700

    computed donor richness

--- workflow/analysis/scratch/031926-getDonorRichness/031926-getDonorRichness.R
+++ workflow/analysis/scratch/031926-getDonorRichness/031926-getDonorRichness.R
@@ -1,4 +1,10 @@
 source("~/Documents/GitHub/abx-response-invitro/workflow/analysis/plotDefaults.R")
 source("~/Documents/GitHub/abx-response-invitro/workflow/analysis/scratch/072125-loade0029Data/072125-loade0029Data.R")
-source("~/Documents/GitHub/abx-response-invitro/workflow/analysis/scratch/072125-getColonization/072125-getColonization.R")
-source("~/Documents/GitHub/abx-response-invitro/workflow/analysis/scratch/072225-getColonizationProportion/072225-getColonizationProportion.R")
+
+
+# Compute species richness
+
+donor_richness <- single_donor_ASVs %>% 
+  filter(relAbundance > limit_of_detection) %>% 
+  group_by(replicate, passage, biosample1) %>% 
+  summarize(richness = n())
